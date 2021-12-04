d7e18eb3c506ac268625e71a5a28d385226ef154
Brad <bradley@monzo.com>
2021-12-04 13:27:14 +0000

    Golf

--- src/day04/day4.kt
+++ src/day04/day4.kt
@@ -8,13 +8,14 @@ fun main() {
     val guesses = input[0].split(",").map { it.toInt() }
     val boards = input.subList(1, input.size).map { Board(it) }
 
+    val completed = BooleanArray(boards.size)
     var lastScore = 0
 
     for (guess in guesses) {
-        for (board in boards) {
-            val unmarkedSum = board.guess(guess)
-            if (unmarkedSum != null) {
-                lastScore = unmarkedSum * guess
+        for ((i, board) in boards.withIndex()) {
+            if (!completed[i] && board.guess(guess)) {
+                completed[i] = true
+                lastScore = board.remainingSum() * guess
             }
         }
     }
@@ -23,64 +24,34 @@ fun main() {
 }
 
 class Board(data: String) {
-    private val grid: Array<Array<Int>>
-    private val map = Array<Pair<Int, Int>?>(100) { null }
+    private val indexMap = Array<Pair<Int, Int>?>(100) { null }
+    private val rowCounts = IntArray(5)
+    private val colCounts = IntArray(5)
     private var unmarkedSum = 0
-    var won = false
-        private set
 
     init {
         val rows = data.split("\n")
-
-        grid = Array(rows.size) { row ->
-            val nums = rows[row].trim().split(Regex("\\s+"))
-            Array(nums.size) { col ->
-                val value = nums[col].toInt()
-                map[value] = Pair(row, col)
-                unmarkedSum += value
-                value
+        for (row in 0..4) {
+            val nums = rows[row].split(" ").filter { it.isNotEmpty() }
+            for (col in 0..4) {
+                val num = nums[col].toInt()
+                indexMap[num] = Pair(row, col)
+                unmarkedSum += num
             }
         }
     }
 
-    fun guess(value: Int): Int? {
-        if (won) return null
+    fun guess(value: Int): Boolean {
+        val (row, col) = indexMap[value] ?: return false
+        indexMap[value] = null
 
-        val (row, col) = map[value] ?: return null
         unmarkedSum -= value
-        map[value] = null
 
-        grid[row][col] = Int.MIN_VALUE
+        rowCounts[row]++
+        colCounts[col]++
 
-        if (checkWin(row, col)) {
-            won = true
-            return unmarkedSum
-        }
-
-        return null
+        return rowCounts[row] == 5 || colCounts[col] == 5
     }
 
-    private fun checkWin(row: Int, col: Int): Boolean {
-        var win = true
-
-        for (i in grid.indices) {
-            if (grid[i][col] != Int.MIN_VALUE) {
-                win = false
-                break
-            }
-        }
-        if (win) {
-            return true
-        }
-
-        win = true
-        for (i in grid[row].indices) {
-            if (grid[row][i] != Int.MIN_VALUE) {
-                win = false
-                break
-            }
-        }
-
-        return win
-    }
+    fun remainingSum() = unmarkedSum
 }
