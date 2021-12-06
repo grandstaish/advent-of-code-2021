b8655d5ac38c6df5109a55e330d7276f8ae669b8
Brad <bradley@monzo.com>
2021-12-06 11:37:20 +0000

    Day 6

---
+++ src/day06/day6.kt
@@ -0,0 +1,27 @@
+package day06
+
+import java.io.File
+
+fun main() {
+    val input = File("src/day06/input.txt").readText().split(",")
+
+    val fishies = LongArray(9)
+
+    for (fish in input) {
+        fishies[fish.toInt()]++
+    }
+
+    for (day in 0 until 256) {
+        var babies = 0L
+        if (fishies[0] > 0) {
+            babies += fishies[0]
+        }
+        for (i in 0 until 8) {
+            fishies[i] = fishies[i + 1]
+        }
+        fishies[6] += babies
+        fishies[8] = babies
+    }
+
+    println(fishies.sum())
+}
