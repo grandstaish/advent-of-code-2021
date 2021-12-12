7209fceca7b156fd8b3cd50a4e028710614932d6
Brad <bradley@monzo.com>
2021-12-12 16:24:41 +0000

    Thanks again Phil

--- src/day12/day12.kt
+++ src/day12/day12.kt
@@ -1,39 +1,52 @@
 package day12
 
 import java.io.File
+import kotlin.system.measureTimeMillis
 
 fun main() {
-    val start = parseInput(File("src/day12/input.txt").readLines())
-    println(dfs(start, emptySet(), false))
+    val start: Node
+    val parseTime = measureTimeMillis {
+        start = parseInput(File("src/day12/input.txt"))
+    }
+    val result: Int
+    val dfsTime = measureTimeMillis {
+        result = dfs(start, false)
+    }
+    println("Parse time: ${parseTime}ms")
+    println("Dfs time: ${dfsTime}ms")
+    println("Result: $result")
 }
 
-private fun dfs(node: Node, visited: Set<String>, hasMadeException: Boolean): Int {
-    if (node.value == "end") return 1
+private fun dfs(node: Node, hasMadeException: Boolean): Int {
+    if (node.isEnd) return 1
 
     var exception = hasMadeException
-    if (visited.contains(node.value)) {
-        if (hasMadeException || node.value == "start") return 0
+    if (!node.isLarge && node.frequency > 0) {
+        if (hasMadeException || node.isStart) return 0
         exception = true
     }
 
+    node.frequency++
+
     var paths = 0
-    val nextVisited = if (node.isLarge) visited else visited + node.value
     for (child in node.children) {
-        paths += dfs(child, nextVisited, exception)
+        paths += dfs(child, exception)
     }
+
+    node.frequency--
 
     return paths
 }
 
-private fun parseInput(input: List<String>): Node {
+private fun parseInput(file: File): Node {
     val nodes = mutableMapOf<String, Node>()
-    for (line in input) {
+    for (line in file.readLines()) {
         val (s, e) = line.split("-")
         val sNode = nodes.getOrPut(s) {
-            Node(value = s, isLarge = s[0].isUpperCase())
+            Node(value = s, isLarge = s[0].isUpperCase(), isEnd = s == "end", isStart = s == "start")
         }
         val eNode = nodes.getOrPut(e) {
-            Node(value = e, isLarge = e[0].isUpperCase())
+            Node(value = e, isLarge = e[0].isUpperCase(), isEnd = e == "end", isStart = e == "start")
         }
         sNode.children.add(eNode)
         eNode.children.add(sNode)
@@ -43,7 +56,10 @@ private fun parseInput(input: List<String>): Node {
 
 private data class Node(
     val value: String,
-    val isLarge: Boolean
+    val isLarge: Boolean,
+    val isEnd: Boolean,
+    val isStart: Boolean
 ) {
+    var frequency = 0
     val children = mutableListOf<Node>()
 }
